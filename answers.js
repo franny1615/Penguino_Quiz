dce0765a6f32ed72b332330989203ef04dcf0ce9
francisco <franke1615@gmail.com>
2017-09-01 09:57:32 -0700

    added second question and a table for the rest of questions?

--- answers.js
+++ answers.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("#y").click(function(){
 		$("#yes").fadeIn();
 		$("#no").fadeOut();
+		
 	})
 });
 
@@ -9,5 +10,12 @@ $(document).ready(function(){
 	$("#n").click(function(){
 		$("#no").fadeIn();
 		$("#yes").fadeOut();
+	})
+});
+
+$(document).ready(function(){
+	$(".nextBtn").click(function(){
+		$("#q1").fadeOut();
+		$("#q2").fadeIn();
 	})
 });
